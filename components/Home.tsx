1e91e2b1cda4c7ab02977e7d64ecfd9d8cef6362
minwoogramer <01082249606nmw@gmail.com>
2023-01-08 21:07:25 +0900

    Home test#2

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,22 +2,21 @@ import Link from "next/link";
 
 export const HomeComponent = () => {
   return (
-    <div className="flex md:w-[500px md:h-auto]">
+    <div className="flex">
       <div
         id="home"
-        className="flex-col w-full h-[90%] bg-firstbg bg-no-repeat  bg-auto bg-center m-auto"
+        className="flex-col w-full h-[90%] items-center justify-center"
       >
-        <div className="flex items-center w-full h-screen justify-center">
+        <div className="flex items-center w-full h-screen justify-center bg-firstbg  bg-no-repeat bg-cover m-auto">
           <div className="mb-[150px]">
-            <div className=" flex-col ml-11  w-128 h-auto ">
-              <h1 className="text-[9rem] flex font-bold tracking-tight sm:text-center text-white font-serif items-center  justify-center ">
+            <div className=" flex-col ml-11  w-full h-auto ">
+              <h1 className="text-[7em]  flex font-bold tracking-tight sm:text-center text-white font-serif items-center  justify-center ">
                 Suino
               </h1>
-
               <h2 className="text-[2.2rem] text-white sm:text-center font-Diplomata italic  flex items-center  justify-center">
                 Who the best Rich Boss?
               </h2>
-              <div className="h-[50px] mt-8 flex gap-x-5 sm:justify-center flex items-center  justify-center">
+              <div className="h-[50px] mt-8 gap-x-5 sm:justify-center flex items-center  justify-center">
                 <Link
                   href="game"
                   className="flex justify-center items-center rounded-lg bg-yellow-600 px-4 py-1.5 text-[20px]  leading-7 text-white shadow-sm  hover:bg-yellow-800"
