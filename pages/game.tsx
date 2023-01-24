a2f6eebff4c17cfb6a192eaf05cbdabd8bc5b5b7
minwoogramer <01082249606nmw@gmail.com>
2023-01-24 20:49:49 +0900

    Update game.tsx

--- pages/game.tsx
+++ pages/game.tsx
@@ -71,7 +71,6 @@ const GamePage = (props: {
     }[]
   >(props.data);
   useEffect(() => {
-    getServerSideProps();
     fetch("http://34.125.37.158:3306")
       .then((x) => x.json())
       .then((x) => setDashs(x));
